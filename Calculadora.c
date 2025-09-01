deeeb144a40dc3329f835e3313ac4c963be54a74
Eduardo <55516976+Eddy-Marin@users.noreply.github.com>
2025-09-01 15:41:51 -0600

    Add: principal menu

---
+++ Calculadora.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+
+
+int main() {
+	int opcion;
+	float num1, num2, resultado;
+	do {
+		printf("\nCalculadora básica\n");
+		printf("1. Suma\n");
+		printf("2. Resta\n");
+		printf("3. Multiplicación\n");
+		printf("4. División\n");
+		printf("5. Salir\n");
+		printf("Seleccione una opción: ");
+		scanf("%d", &opcion);
+
+		if (opcion >= 1 && opcion <= 4) {
+			printf("Ingrese el primer número: ");
+			scanf("%f", &num1);
+			printf("Ingrese el segundo número: ");
+			scanf("%f", &num2);
+		}
+
+		switch (opcion) {
+			case 1:
+				resultado = suma(num1, num2);
+				printf("Resultado: %.2f\n", resultado);
+				break;
+			case 2:
+				resultado = resta(num1, num2);
+				printf("Resultado: %.2f\n", resultado);
+				break;
+			case 3:
+				resultado = multiplicacion(num1, num2);
+				printf("Resultado: %.2f\n", resultado);
+				break;
+			case 4:
+				resultado = division(num1, num2);
+				printf("Resultado: %.2f\n", resultado);
+				break;
+			case 5:
+				printf("Saliendo...\n");
+				break;
+			default:
+				printf("Opción no válida. Intente de nuevo.\n");
+		}
+	} while (opcion != 5);
+	return 0;
+}
